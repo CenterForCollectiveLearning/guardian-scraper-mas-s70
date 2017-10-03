a25cdd4d0c60445bd1f335783ec647251576b52f
kzh <kzh@mit.edu>
2017-10-03 11:40:48 -0400

    Change font. Add search term length check

--- main.js
+++ main.js
@@ -8,48 +8,85 @@ Array.prototype.pairs = function (func) {
 	}
 }
 
-function runQuery() {
-	console.log("starting runQuery");
-	var queryText = $("#query-data").val();
-	console.log(queryText);
-	var queryArray = queryText.split("\n");
-	queryArray.forEach(function (queryOne) {
-	  var url = "https://content.guardianapis.com/search";
-	  url += '?' + $.param({
-	    'api-key': "89264c96-0794-4cb6-b61d-d87ebbc8c3f6",
-	    'q': queryOne
-	  });
-	  $.ajax({
-	    url: url,
-	    method: 'GET',
-	  }).done(function(result) {
-	    window.data.push([ queryOne, result.response.total ])
-	    $("#results-single").append("<p>" + queryOne + " : " + result.response.total + "</p>");
-	  }).fail(function(err) {
-	    console.log(err);
-	  });
-	})
+Array.prototype.triplets = function (func) {
+	for (var i = 0; i < this.length - 1; i++) {
+	    for (var j = i; j < this.length - 1; j++) {
+	    	for (var k = j; k < this.length - 1; k++) {
+	        func([this[i], this[j+1], this[k+2]]);
+	      }
+	    }
+	}
+}
 
-	queryPairs = queryArray.pairs( function (pair) {
-	  console.log(pair[0], pair[1])
-	  var queryPair = pair[0] + " " + pair[1];
-	  console.log(queryPair);
-	  var url = "https://content.guardianapis.com/search";
-	  url += '?' + $.param({
-	    'api-key': "89264c96-0794-4cb6-b61d-d87ebbc8c3f6",
-	    'q': queryPair
-	  });
-	  console.log(url)
-	  $.ajax({
-	    url: url,
-	    method: 'GET',
-	  }).done(function(result) {
-	    window.data.push([ queryPair, result.response.total ])            
-	    $("#results-pairs").append("<p>" + queryPair + " : " + result.response.total + "</p>");
-	  }).fail(function(err) {
-	    console.log(err);
-	  });
-	})
+function runQuery() {
+	var queryText = $("#query-data").val();
+	var queryArray = queryText.split("\n");
+	var queryLength = queryArray.length;
+
+	if (queryLength >= 1) {
+		queryArray.forEach(function (queryOne) {
+		  var url = "https://content.guardianapis.com/search";
+		  url += '?' + $.param({
+		    'api-key': "89264c96-0794-4cb6-b61d-d87ebbc8c3f6",
+		    'q': queryOne
+		  });
+		  $.ajax({
+		    url: url,
+		    method: 'GET',
+		  }).done(function(result) {
+		    window.data.push([ queryOne, result.response.total ])
+		    $("#results-single").append("<p>" + queryOne + " : " + result.response.total + "</p>");
+		  }).fail(function(err) {
+		    console.log(err);
+		  });
+		})	
+	}
+
+	if (queryLength >= 2) {
+		queryPairs = queryArray.pairs( function (pair) {
+		  console.log(pair[0], pair[1])
+		  var queryPair = pair[0] + " " + pair[1];
+		  console.log(queryPair);
+		  var url = "https://content.guardianapis.com/search";
+		  url += '?' + $.param({
+		    'api-key': "89264c96-0794-4cb6-b61d-d87ebbc8c3f6",
+		    'q': queryPair
+		  });
+		  console.log(url)
+		  $.ajax({
+		    url: url,
+		    method: 'GET',
+		  }).done(function(result) {
+		    window.data.push([ queryPair, result.response.total ])            
+		    $("#results-pairs").append("<p>" + queryPair + " : " + result.response.total + "</p>");
+		  }).fail(function(err) {
+		    console.log(err);
+		  });
+		})
+	}
+
+	if (queryLength >= 3) {
+		queryTriplets = queryArray.triplets( function (triplet) {
+		  console.log(triplet[0], triplet[1], triplet[2])
+		  var queryTriplet = triplet[0] + " " + triplet[1] + " " + triplet[2];
+		  console.log(queryTriplet);
+		  var url = "https://content.guardianapis.com/search";
+		  url += '?' + $.param({
+		    'api-key': "89264c96-0794-4cb6-b61d-d87ebbc8c3f6",
+		    'q': queryTriplet
+		  });
+		  console.log(url)
+		  $.ajax({
+		    url: url,
+		    method: 'GET',
+		  }).done(function(result) {
+		    window.data.push([ queryTriplet, result.response.total ])            
+		    $("#results-triplets").append("<p>" + queryTriplet + " : " + result.response.total + "</p>");
+		  }).fail(function(err) {
+		    console.log(err);
+		  });
+		})	
+	}
 
 	document.getElementById('download-button').style.display = 'block'
 }
